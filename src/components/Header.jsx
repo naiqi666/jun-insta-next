e916538212b0af0c28a35503f6e9494d3e52131e
JUNPUYIN WEI NAME <naiqi666@gmail.com>
2024-08-09 05:11:10 -0400

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="shadow-sm  border-b sticky top-0 bg-white z-30 p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        {/* {logo} */}
+        <Link href="/" className="hidden lg:inline-flex ">
+          <Image
+            src="/Instagram_logo_black.webp"
+            width={96}
+            height={96}
+            alt="Instagramlogo"
+          />
+        </Link>
+
+        <Link href="/" className="lg:hidden">
+          <Image
+            src="/800px-Instagram_logo_2016.webp"
+            width={40}
+            height={40}
+            alt="Instagramlogo"
+          />
+        </Link>
+        {/* {search input} */}
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
+        />
+        {/* {menu items} */}
+        <button className="text-sm font-semibold text-blue-500">Log In</button>
+      </div>
+    </div>
+  );
+}
